6c3c6e102d97d6f4e4234200df11e502e51547c4
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2022-04-24 21:42:40 +0300

    task9

--- src/js/task-09.js
+++ src/js/task-09.js
@@ -1,3 +1,17 @@
 function getRandomHexColor() {
-  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
 }
+const changeBodyColor = document.querySelector('body');
+const btnChange = document.querySelector('.change-color');
+const outputColor = document.querySelector('.color');
+
+btnChange.addEventListener('click', changeBgColor);
+function changeBgColor() {
+  outputColor.textContent = getRandomHexColor();
+  changeBodyColor.style.backgroundColor = outputColor.textContent;
+}
+btnChange.style.borderRadius = '25px';
+btnChange.style.backgroundColor = 'transparent';
+btnChange.style.borderColor = '#000000';

e81d2303306e3daa894c16d7d680f23e5f3e79e1
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2022-04-25 13:13:14 +0300

    fixed task6

--- js/task-06.js
+++ js/task-06.js
@@ -3,8 +3,8 @@ const inputEl = document.querySelector('input');
 inputEl.addEventListener('blur', (event) => {
   event.preventDefault();
   if (event.currentTarget.value.length !== Number(inputEl.dataset.length)) {
-    inputEl.classList.add('invalid');
     inputEl.classList.remove('valid');
+    inputEl.classList.add('invalid');
   } else {
     inputEl.classList.remove('invalid');
     inputEl.classList.add('valid');

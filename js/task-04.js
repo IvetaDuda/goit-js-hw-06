a01df96df7d64f61500f4c8d69023e8e9bbe9e12
IvetaDuda <92565279+IvetaDuda@users.noreply.github.com>
2022-04-25 21:30:24 +0300

    fixed task 4

--- js/task-04.js
+++ js/task-04.js
@@ -2,7 +2,7 @@ const btnDecrement = document.querySelector('button[data-action="decrement"]');
 const btnIncrement = document.querySelector('button[data-action="increment"]');
 const value = document.querySelector('#value');
 
-let number = (Number = 0);
+let number = 0;
 const onBtnDecrement = () => {
   value.textContent = number -= 1;
 };
